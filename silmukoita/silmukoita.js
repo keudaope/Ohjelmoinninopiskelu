57644b7f166137bed01645273798589cd5556466
Jyri Lindroos <jyri.lindroos@keuda.fi>
2021-03-10 09:42:25 +0200

    silmukoita / Jyri Lindroos / 10.3.2021

---
+++ silmukoita/silmukoita.js
@@ -0,0 +1,16 @@
+var lause = "Olipa kerran onnimanni";
+/*for(var x = lause.length-1; x >= 0; x--)
+{
+  console.log(lause[x]);
+}
+var x = lause.length-1;
+while(x >= 0 )
+{
+  console.log(lause[x]);
+  x--;
+}*/
+var x = lause.length;
+do {
+  console.log(lause[x]);
+  x--;
+} while (x >= 0 );
